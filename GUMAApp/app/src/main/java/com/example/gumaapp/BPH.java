471f8183b5893ab46eeecf818fe9dbaa76c794b2
Muhammad Akmal <muhammadakmal@apps.ipb.ac.id>
2021-05-03 00:15:25 +0700

    baruuuu

---
+++ GUMAApp/app/src/main/java/com/example/gumaapp/BPH.java
@@ -0,0 +1,14 @@
+package com.example.gumaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BPH extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b_p_h);
+    }
+}
---
+++ GUMAApp/app/src/main/java/com/example/gumaapp/Epsilon.java
@@ -0,0 +1,14 @@
+package com.example.gumaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Epsilon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_epsilon);
+    }
+}
