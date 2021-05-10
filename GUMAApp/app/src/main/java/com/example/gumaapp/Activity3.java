8310d6f0f137a13ac45c25e46cf3528638cd0f90
MIRZAIPB56 <mirzafarhanazhari@apps.ipb.ac.id>
2021-05-10 11:37:53 +0700

    baru

--- GUMAApp/app/src/main/java/com/example/gumaapp/Activity3.java
+++ GUMAApp/app/src/main/java/com/example/gumaapp/Activity3.java
@@ -1,14 +1,137 @@
 package com.example.gumaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Person;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Activity3 extends AppCompatActivity {
+    private ImageButton imageButton,imageButton2,imageButton3,imageButton4,imageButton5,imageButton6,imageButton7,imageButton8,imageButton9,imageButton10,imageButton11,imageButton12;
+    ConstraintLayout ConstraintLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_3);
+        ConstraintLayout=findViewById(R.id.container);
+
+
+        imageButton = (ImageButton) findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity3.this,Activity3.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        imageButton2 = (ImageButton) findViewById(R.id.imageButton2);
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity3.this,Activity3.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        imageButton3 = (ImageButton) findViewById(R.id.imageButton3);
+        imageButton3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity3.this,schedule.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        imageButton4 = (ImageButton) findViewById(R.id.imageButton4);
+        imageButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity3.this,Activity3.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        imageButton5 = (ImageButton) findViewById(R.id.imageButton5);
+        imageButton5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity3.this,BPH.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        imageButton6 = (ImageButton) findViewById(R.id.imageButton6);
+        imageButton6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity3.this,BPG.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        imageButton7 = (ImageButton) findViewById(R.id.imageButton7);
+        imageButton7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity3.this,Invers.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        imageButton8 = (ImageButton) findViewById(R.id.imageButton8);
+        imageButton8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity3.this,Mantissa.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        imageButton9 = (ImageButton) findViewById(R.id.imageButton9);
+        imageButton9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity3.this,Pearson.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        imageButton10 = (ImageButton) findViewById(R.id.imageButton10);
+        imageButton10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity3.this,Median.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        imageButton11 = (ImageButton) findViewById(R.id.imageButton11);
+        imageButton11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity3.this,Epsilon.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        imageButton12 = (ImageButton) findViewById(R.id.imageButton12);
+        imageButton12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity3.this,Bisection.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
-}
+    }
